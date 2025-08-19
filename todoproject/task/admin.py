52ec58611947755c756d9cbc0d70293a0eb0adf2
EvgeniyPosohin <mad.house@bk.ru>
2025-08-19 15:09:04 +0500

    add models

--- todoproject/task/admin.py
+++ todoproject/task/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task, Category
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'status', 'priority', 'date', 'create_at')
+    list_filter = ('status', 'priority', 'category')
+    search_fields = ('title', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+    search_fields = ('name',)
--- todoproject/task/models.py
+++ todoproject/task/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    class Meta:
+        unique_together = ('owner', 'name')
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        TODO = 'todo', 'к выполнению'
+        IN_PROGRESS = 'in_progress', 'в процессе'
+        DONE = 'done', 'Сделано'
+
+    class Priority(models.IntegerChoices):
+        LOW = 1, 'низкий'
+        MEDIUM = 2, 'средний'
+        HIGH = 3, 'высокий'
+
+    title = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    description = models.TextField(blank=True)
+
+    category = models.ForeignKey(
+        Category, null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='tasks'
+    )
+    status = models.CharField(max_length=20, choices=Status.choices,
+                              default=Status.TODO
+                              )
+    priority = models.IntegerField(choices=Priority.choices,
+                                   default=Priority.MEDIUM)
+    date = models.DateField(null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['status', '-priority', 'date', '-create_at']
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def is_overdue(self):
+        return (
+            self.date and self.status != self.Status.DONE
+            and self.date < timezone.localdate()
+        )
